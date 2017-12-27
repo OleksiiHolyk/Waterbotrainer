cc28bd963ff50f064cc9d7bf09a63594b07c61be
Oleksii <oleksii.holyk@outlook.com>
2017-12-27 23:15:22 +0200

    remove spring scheduler added quartz scheduler

--- src/main/java/ua/oleksiiholyk/service/Reminder.java
+++ src/main/java/ua/oleksiiholyk/service/Reminder.java
@@ -25,49 +25,4 @@ public class Reminder {
     }
 
 
-   /*
-    second, minute, hour, day of month, month, day(s) of week
-    "0 0 * * * *" = the top of every hour of every day.
-    "10 * * * * *" = every ten seconds.
-    "0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.
-    "0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.
-    "0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays
-    "0 0 0 25 12 ?" = every Christmas Day at midnight
-    */
-   /* @Scheduled(cron = "0 * * * * ?")
-    public void scheduleTaskWithCronExpression() {
-        logger.info("Cron Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
-    }
-
-    @Scheduled(fixedRate = 2000)
-    public void scheduleTaskWithFixedRate() {
-        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
-    }
-
-    @Scheduled(fixedDelay = 2000)
-    public void scheduleTaskWithFixedDelay() {
-        logger.info("Fixed Delay Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException ex) {
-            logger.error("Ran into an error {}", ex);
-            throw new IllegalStateException(ex);
-        }
-    }
-
-    @Scheduled(fixedRate = 2000, initialDelay = 5000)
-    public void scheduleTaskWithInitialDelay() {
-        logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
-    }*/
-
-
-   /* @Scheduled(fixedRate = 6000)
-    public void scheduleOnceMinute(){
-        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
-    }*/
-
-    @Scheduled(cron = "0 * * * * ?")
-    public void scheduleTaskWithCronExpression() {
-        logger.info("Cron Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
-    }
 }
